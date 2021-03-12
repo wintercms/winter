72f87f2dad455dff22eacbc8cb27327816d859ac
Luke Towers <github@luketowers.ca>
2021-03-12 09:27:01 -0600

    improve asset aliasing

--- modules/system/traits/AssetMaker.php
+++ modules/system/traits/AssetMaker.php
@@ -115,6 +115,14 @@ trait AssetMaker
             $name = $this->combineAssets($name, $this->getLocalPath($this->assetPath));
         }
 
+        // Alias october.* assets to winter.*
+        if (str_contains($name, 'js/october.')) {
+            $winterPath = str_replace('js/october.', 'js/winter.', $name);
+            if (file_exists(base_path(ltrim(parse_url($winterPath, PHP_URL_PATH), '/')))) {
+                $name = $winterPath;
+            }
+        }
+
         $jsPath = $this->getAssetPath($name);
 
         if (isset($this->controller)) {
@@ -191,14 +199,6 @@ trait AssetMaker
      */
     protected function addAsset(string $type, string $path, array $attributes)
     {
-        // Alias october.* assets to winter.*
-        if (str_contains($path, 'october.')) {
-            $winterPath = str_replace('october.', 'winter.', $path);
-            if (file_exists(base_path(ltrim(parse_url($winterPath, PHP_URL_PATH), '/')))) {
-                $path = $winterPath;
-            }
-        }
-
         if (!in_array($path, $this->assets[$type])) {
             /**
              * @event system.assets.beforeAddAsset
