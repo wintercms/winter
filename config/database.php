294dc1317b7fe6ec087ce8f429115e2ba52d9fc8
Ben Thomson <git@alfreido.com>
2021-05-16 09:32:59 +0800

    Set SQLite foreign key constraints to true (default in L6)

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             // 'url' => env('DATABASE_URL'),
             'database' => base_path('storage/database.sqlite'),
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'foreign_key_constraints' => true,
         ],
 
         'mysql' => [
