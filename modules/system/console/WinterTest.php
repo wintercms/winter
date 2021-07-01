9dc7e526bc3e0d3b5c8bf4d076febedec67e23ba
RomainMazB <romainmazb@gmail.com>
2021-07-01 22:28:56 +0200

    Gives the ability to pass a custom configuration file, and now fallback to phpunit.xml.dist file

--- modules/system/console/WinterTest.php
+++ modules/system/console/WinterTest.php
@@ -21,7 +21,7 @@ class WinterTest extends Command
      */
     protected $name = 'winter:test';
 
-    protected $signature = 'winter:test {plugin?}';
+    protected $signature = 'winter:test {plugin?} {--configuration=}';
 
     /**
      * The console command description.
@@ -49,47 +49,55 @@ class WinterTest extends Command
 
     /**
      * Execute the console command.
-     * @return int
+     * @return int|void
      */
     public function handle()
     {
         // If the plugin argument is set, search for it and run its tests
-        $pluginName = $this->argument('plugin');
-        if (!is_null($pluginName)) {
+        $pluginArgument = $this->argument('plugin');
+        if (!is_null($pluginArgument)) {
             $pluginManager = PluginManager::instance();
             $pluginName = $pluginManager->normalizeIdentifier($pluginArgument);
 
             if (!$pluginManager->exists($pluginName)) {
                 $this->error(sprintf('Plugin "%s" not found.', $pluginName));
+
+                return;
             }
 
-            $pluginDir = $pluginManager->getPluginPath($pluginName);
-
-            $pluginPHPUnitXMLFile = $pluginDir . '/phpunit.xml';
-            if (!file_exists($pluginPHPUnitXMLFile)) {
-                throw new \InvalidArgumentException(sprintf('phpunit.xml file not found in "%s".', $pluginDir));
-            }
-
-            /*
-             * Test plugin
-             */
-            $this->exec[] = "--configuration=$pluginPHPUnitXMLFile";
-            $this->info(sprintf('Running  plugin\'s tests: %s.', $pluginName));
+            $testsPath = $pluginManager->getPluginPath($pluginName);
         }
 
         // Run the core Winter CMS tests
         else {
-            $this->info('Running  Winter\'s core tests.');
+            $testsPath = base_path();
         }
 
-        // Add eventual PHPUnit native options
-        $options = array_slice($_SERVER['argv'], $this->argument('plugin') ? 3 : 2);
+        $phpunitXMLFile = $this->lookForPHPUnitXMLFile($testsPath);
 
-        $this->exec = array_merge($this->exec, $options);
+        if (!$phpunitXMLFile) {
+            $this->error(sprintf('Configuration file not found in "%s".', $testsPath));
+
+            return;
+        }
+
+        $this->exec[] = "--configuration=$phpunitXMLFile";
+
+        // Add eventual PHPUnit native options
+        $phpunitArguments = $this->getPHPUnitArguments();
+
+        $this->exec = array_merge($this->exec, $phpunitArguments);
 
         $process = (new Process($this->exec))->setTimeout(null);
 
         try {
+            $runningTestsMessage =
+                isset($pluginName) ? sprintf('Running  plugin\'s tests: %s.', $pluginName)
+                    : 'Running  Winter\'s core tests.';
+
+            $this->info($runningTestsMessage);
+            $this->info(sprintf('Configuration file used: %s.', $phpunitXMLFile));
+
             return $process->run(function ($type, $line) {
                 $this->output->write($line);
             });
@@ -109,5 +117,67 @@ class WinterTest extends Command
         return [
             ['plugin', InputArgument::OPTIONAL, 'The name of the plugin. Eg: AuthorName.PluginName'],
         ];
+    }
+
+    /**
+     * Search for the config file to use
+     * and gives an opportunity to change it if not found,
+     * priority order is: --configuration option, phpunit.xml then phpunit.xml.dist
+     * @param string|bool $directory
+     */
+    protected function lookForPHPUnitXMLFile(string $directory)
+    {
+        $configurationOption = $this->option('configuration');
+
+        // If the user explicitly provided a custom configuration file
+        // Make sure it exists or abort as its clear we don't want to run default phpunit files
+        if ($configurationOption) {
+            $optionFilePath = $directory . DIRECTORY_SEPARATOR . $configurationOption;
+
+            if (file_exists($optionFilePath)) {
+                return $optionFilePath;
+            }
+
+            return false;
+        }
+
+        // If a phpunit.xml file exists, returns its path
+        $distFilePath = $directory . DIRECTORY_SEPARATOR . 'phpunit.xml';
+        if (file_exists($distFilePath)) {
+            return $distFilePath;
+        }
+
+        // Fallback to phpunit.xml.dist file path if it exists
+        $configFilePath = $directory . DIRECTORY_SEPARATOR . 'phpunit.xml.dist';
+        if (file_exists($configFilePath)) {
+            return $configFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Strips out this commands arguments and options in order to return arguments/options for PHPUnit.
+     *
+     * @return array
+     */
+    protected function getPHPUnitArguments()
+    {
+        $arguments = $_SERVER['argv'];
+
+        // First two are always "artisan" and "winter:test"
+        $arguments = array_slice($arguments, 2);
+
+        // Strip plugin argument
+        if ($this->argument('plugin')) {
+            array_shift($arguments);
+        }
+
+        // Strip "--configuration"
+        if ($this->option('configuration')) {
+            array_shift($arguments);
+        }
+
+        return $arguments;
     }
 }
