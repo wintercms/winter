a337647a32cb1be2a26dacd61b279f3bc24019e0
Ben Thomson <git@alfreido.com>
2021-09-09 12:47:23 +0800

    Fix alignment of Object.assign polyfill

--- modules/system/assets/js/framework/base.js
+++ modules/system/assets/js/framework/base.js
@@ -243,32 +243,32 @@
     if (typeof Object.assign !== 'function') {
         // Must be writable: true, enumerable: false, configurable: true
         Object.defineProperty(Object, 'assign', {
-          value: function assign(target, varArgs) { // .length of function is 2
-            if (target === null || target === undefined) {
-              throw new TypeError('Cannot convert undefined or null to object')
-            }
-
-            var to = Object(target)
-
-            for (var index = 1; index < arguments.length; index++) {
-              var nextSource = arguments[index];
-
-              if (nextSource !== null && nextSource !== undefined) {
-                for (var nextKey in nextSource) {
-                  // Avoid bugs when hasOwnProperty is shadowed
-                  if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                    to[nextKey] = nextSource[nextKey]
-                  }
+            value: function assign(target) {
+                if (target === null || target === undefined) {
+                    throw new TypeError('Cannot convert undefined or null to object')
                 }
-              }
-            }
-            return to
-          },
-          writable: true,
-          configurable: true,
-        })
-      }
 
+                var to = Object(target)
+
+                for (var index = 1; index < arguments.length; index++) {
+                    var nextSource = arguments[index];
+
+                    if (nextSource !== null && nextSource !== undefined) {
+                        for (var nextKey in nextSource) {
+                            // Avoid bugs when hasOwnProperty is shadowed
+                            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                                to[nextKey] = nextSource[nextKey]
+                            }
+                        }
+                    }
+                }
+
+                return to
+            },
+            writable: true,
+            configurable: true,
+        })
+    }
 
     // Define Winter JS framework in global namespace
     window.winter = new WinterJsFramework()
