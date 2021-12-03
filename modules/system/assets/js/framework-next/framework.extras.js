4a2ef4ce17c0da5238dce3223abcb921fd9567f1
Ben Thomson <git@alfreido.com>
2021-12-03 13:49:37 +0800

    Rebuild

--- modules/system/assets/js/framework-next/framework.extras.js
+++ modules/system/assets/js/framework-next/framework.extras.js
@@ -1,9 +1,11 @@
 import Flash from './extras/Flash';
 import FlashListener from './extras/FlashListener';
 import Transition from './extras/Transition';
+import AttachLoading from './extras/AttachLoading';
 
 ((winter) => {
     winter.addModule('transition', Transition)
     winter.addModule('flash', Flash);
     winter.addModule('flashListener', FlashListener);
+    winter.addModule('attachLoading', AttachLoading);
 })(window.winter);
