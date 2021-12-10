0b6646213f7e0d996d4369c619a0af3b43dd39dc
Luke Towers <github@luketowers.ca>
2021-12-10 15:57:41 -0600

    Remove mentions of Rackspace and isolate the storage directory used by tests by default

--- config/testing/filesystems.php
+++ config/testing/filesystems.php
@@ -45,8 +45,8 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root'   => storage_path('app'),
-            'url'    => '/storage/app',
+            'root'   => base_path('tests/storage'),
+            'url'    => '/tests/storage',
         ],
 
         's3' => [
