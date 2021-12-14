099f7312901366e91ffe96a022c5ca7527a40c38
Luke Towers <github@luketowers.ca>
2021-12-14 01:19:01 -0600

    Remove database.useConfigForTesting
    Was redundant, should be using environment config (config/testing/*.php) instead

--- config/database.php
+++ config/database.php
@@ -143,21 +143,4 @@ return [
 
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Use DB configuration for testing
-    |--------------------------------------------------------------------------
-    |
-    | When running plugin tests Winter CMS by default uses SQLite in memory.
-    | You can override this behavior by setting `useConfigForTesting` to true.
-    |
-    | After that Winter CMS will take DB parameters from the config.
-    | If file `/config/testing/database.php` exists, config will be read from it,
-    | but remember that when not specified it will use parameters specified in
-    | `/config/database.php`.
-    |
-    */
-
-    'useConfigForTesting' => false,
-
 ];
