b5bea246a37258cff8bb27fa7ffcbe6a58b729e9
Arvis Lācis <arvis.lacis@gmail.com>
2021-12-18 17:09:58 +0200

    Revert "Fixing datepicker readOnly property #2"
    This reverts commit 732a9bcdcd4d7db5ba2bceddd529a18dbf45c2fe.

--- modules/system/assets/ui/js/datepicker.js
+++ modules/system/assets/ui/js/datepicker.js
@@ -58,14 +58,6 @@
             this.initTimePicker()
         }
 
-        if (this.$datePicker.prop('readonly')) {
-            this.$datePicker.prop('disabled', true)
-        }
-
-        if (this.$timePicker.prop('readonly')) {
-            this.$timePicker.prop('disabled', true)
-        }
-
         if (changeMonitor !== undefined) {
             changeMonitor.resume()
         }
