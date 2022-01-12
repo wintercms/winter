60d70dad07c2ffcda7695a457fc2e086160a8af5
Ben Thomson <git@alfreido.com>
2022-01-12 11:15:11 +0800

    Add stylesheet loader for extras

--- modules/system/assets/js/snowboard/snowboard.extras.js
+++ modules/system/assets/js/snowboard/snowboard.extras.js
@@ -3,8 +3,10 @@ import FlashListener from './extras/FlashListener';
 import Transition from './extras/Transition';
 import AttachLoading from './extras/AttachLoading';
 import StripeLoader from './extras/StripeLoader';
+import StylesheetLoader from './extras/StylesheetLoader';
 
 ((Snowboard) => {
+    Snowboard.addPlugin('extrasStyles', StylesheetLoader);
     Snowboard.addPlugin('transition', Transition);
     Snowboard.addPlugin('flash', Flash);
     Snowboard.addPlugin('flashListener', FlashListener);
