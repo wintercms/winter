83c076c7de266d52de06238d34117f62491aeebc
Ben Thomson <git@alfreido.com>
2022-01-20 09:47:20 +0800

    Add extensibility to cookie utility

--- modules/system/assets/js/snowboard/utilities/Cookie.js
+++ modules/system/assets/js/snowboard/utilities/Cookie.js
@@ -7,17 +7,114 @@ import Singleton from '../abstracts/Singleton';
  * This utility is a thin wrapper around the "js-cookie" library.
  *
  * @see https://github.com/js-cookie/js-cookie
+ * @copyright 2021 Winter.
+ * @author Ben Thomson <git@alfreido.com>
  */
 export default class Cookie extends Singleton {
+    constructor(snowboard) {
+        super(snowboard);
+
+        this.defaults = {
+            expires: null,
+            path: '/',
+            domain: null,
+            secure: false,
+            sameSite: 'Lax',
+        };
+    }
+
+    /**
+     * Set the default cookie parameters for all subsequent "set" and "remove" calls.
+     *
+     * @param {Object} options
+     */
+    defaults(options) {
+        if (typeof options !== 'object') {
+            throw new Error('Cookie defaults must be provided as an object');
+        }
+
+        Object.entries(options).forEach((entry) => {
+            const [key, value] = entry;
+
+            if (this.defaults[key] !== undefined) {
+                this.defaults[key] = value;
+            }
+        });
+    }
+
+    /**
+     * Get the current default cookie parameters.
+     *
+     * @returns {Object}
+     */
+    getDefaults() {
+        const defaults = {};
+
+        Object.entries(this.defaults).forEach((entry) => {
+            const [key, value] = entry;
+
+            if (this.defaults[key] !== null) {
+                defaults[key] = value;
+            }
+        });
+
+        return defaults;
+    }
+
+    /**
+     * Get a cookie by name.
+     *
+     * @param {string} name
+     * @returns {string}
+     */
     get(name) {
-        return BaseCookie.get(name);
+        let value = BaseCookie.get(name);
+
+        // Allow plugins to override the gotten value
+        this.snowboard.globalEvents('cookie.get', name, value, (newValue) => {
+            value = newValue;
+        });
+
+        return value;
     }
 
+    /**
+     * Set a cookie by name.
+     *
+     * You can specify additional cookie parameters through the "options" parameter.
+     *
+     * @param {string} name
+     * @param {string} value
+     * @param {Object} options
+     * @returns {string}
+     */
     set(name, value, options) {
-        return BaseCookie.set(name, value, options);
+        let saveValue = value;
+
+        // Allow plugins to override the value to save
+        this.snowboard.globalEvents('cookie.set', name, value, (newValue) => {
+            saveValue = newValue;
+        });
+
+        return BaseCookie.set(name, saveValue, {
+            ...this.getDefaults(),
+            ...options,
+        });
     }
 
+    /**
+     * Remove a cookie by name.
+     *
+     * You can specify the additional cookie parameters via the "options" parameter.
+     *
+     * @param {string} name
+     * @param {*} options
+     * @returns {void}
+     */
     remove(name, options) {
-        return BaseCookie.remove(name, options);
+        BaseCookie.remove(name, {
+            ...this.getDefaults(),
+            ...options,
+        });
     }
 }
