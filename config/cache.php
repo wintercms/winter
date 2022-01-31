c7ffe2e63a17625cd0e9531f6a8a14693c94565c
Jack Wilkinson <me@jackwilky.com>
2022-01-31 17:18:12 +0000

    Added laravel defaults to cache config

--- config/cache.php
+++ config/cache.php
@@ -56,19 +56,19 @@ return [
         ],
 
         'memcached' => [
-            'driver'        => 'memcached',
-            'persistent_id' => null,
+            'driver' => 'memcached',
+            'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
             'sasl' => [
-                // env('MEMCACHED_USERNAME'),
-                // env('MEMCACHED_PASSWORD'),
+                env('MEMCACHED_USERNAME'),
+                env('MEMCACHED_PASSWORD'),
             ],
             'options' => [
                 // Memcached::OPT_CONNECT_TIMEOUT => 2000,
             ],
             'servers' => [
                 [
-                    'host'   => '127.0.0.1',
-                    'port'   => 11211,
+                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
             ],
@@ -80,12 +80,12 @@ return [
         ],
 
         'dynamodb' => [
-            'driver'   => 'dynamodb',
-            'key'      => '',
-            'secret'   => '',
-            'region'   => 'us-east-1',
-            'table'    => 'cache',
-            'endpoint' => '',
+            'driver'    => 'dynamodb',
+            'key'       => env('AWS_ACCESS_KEY_ID'),
+            'secret'    => env('AWS_SECRET_ACCESS_KEY'),
+            'region'    => env('AWS_DEFAULT_REGION', 'us-east-1'),
+            'table'     => env('DYNAMODB_CACHE_TABLE', 'cache'),
+            'endpoint'  => env('DYNAMODB_ENDPOINT'),
         ],
 
     ],
