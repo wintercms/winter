0c2faa449027e6f44ddff52627825191e57d51c3
Marc Jauvin <marc.jauvin@gmail.com>
2022-02-21 13:17:13 -0500

    register system.beforeRoute and system.route events and use them in cms/backend routes.php

--- modules/cms/routes.php
+++ modules/cms/routes.php
@@ -1,37 +1,39 @@
 <?php
 
-/**
- * Register CMS routes before all user routes.
- */
+Event::listen('system.route', function () {
+    /**
+     * Register CMS routes before all user routes.
+     */
 
-/**
- * @event cms.beforeRoute
- * Fires before cms routes get added
- *
- * Example usage:
- *
- *     Event::listen('cms.beforeRoute', function () {
- *         // your code here
- *     });
- *
- */
-Event::fire('cms.beforeRoute');
+    /**
+     * @event cms.beforeRoute
+     * Fires before cms routes get added
+     *
+     * Example usage:
+     *
+     *     Event::listen('cms.beforeRoute', function () {
+     *         // your code here
+     *     });
+     *
+     */
+    Event::fire('cms.beforeRoute');
 
-/*
- * The CMS module intercepts all URLs that were not
- * handled by the back-end modules.
- */
-Route::any('{slug?}', 'Cms\Classes\CmsController@run')->where('slug', '(.*)?')->middleware('web');
+    /*
+     * The CMS module intercepts all URLs that were not
+     * handled by the back-end modules.
+     */
+    Route::any('{slug?}', 'Cms\Classes\CmsController@run')->where('slug', '(.*)?')->middleware('web');
 
-/**
- * @event cms.route
- * Fires after cms routes get added
- *
- * Example usage:
- *
- *     Event::listen('cms.route', function () {
- *         // your code here
- *     });
- *
- */
-Event::fire('cms.route');
+    /**
+     * @event cms.route
+     * Fires after cms routes get added
+     *
+     * Example usage:
+     *
+     *     Event::listen('cms.route', function () {
+     *         // your code here
+     *     });
+     *
+     */
+    Event::fire('cms.route');
+}, PHP_INT_MIN);
