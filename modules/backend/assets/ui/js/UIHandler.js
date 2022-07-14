e145e29448d1f4bd3a235336e21d990a0d6098ff
Ben Thomson <git@alfreido.com>
2022-07-14 10:31:31 +0800

    Document UI Handler

--- modules/backend/assets/ui/js/UIHandler.js
+++ modules/backend/assets/ui/js/UIHandler.js
@@ -1,16 +1,31 @@
 /**
  * UI Handler.
  *
+ * The UI handler is a single instance that handles connecting elements to their corresponding JS
+ * functionalities and property disposing and destructing plugins as required.
+ *
+ * It also houses any global functionality that might be used throughout the Backend UI.
+ *
  * @copyright 2022 Winter.
  * @author Ben Thomson <git@alfreido.com>
  */
 export default class UIHandler extends Snowboard.Singleton {
+    /**
+     * Dependencies.
+     *
+     * @returns {String[]}
+     */
     dependencies() {
         return [
             'backend.controls.dateTime',
         ];
     }
 
+    /**
+     * Event listeners.
+     *
+     * @returns {Object}
+     */
     listens() {
         return {
             ready: 'ready',
@@ -18,23 +33,51 @@ export default class UIHandler extends Snowboard.Singleton {
         };
     }
 
+    /**
+     * Element matchers.
+     *
+     * Defines the elements that should be handled by each plugin. The plugin name should be the
+     * key, and the CSS selector(s) should be the value.
+     *
+     * @returns {Object}
+     */
     elementMatchers() {
         return {
             'backend.controls.dateTime': 'time[data-datetime-control]',
         };
     }
 
+    /**
+     * Ready event handler.
+     *
+     * This will enable all UI elements in the entire document.
+     */
     ready() {
-        Object.entries(this.elementMatchers()).forEach(([plugin, selector]) => {
-            document.querySelectorAll(selector).forEach((element) => {
-                this.snowboard[plugin](element);
-            });
-        });
+        this.initialisePlugins(document.body);
     }
 
+    /**
+     * AJAX update handler.
+     *
+     * This will enable all UI elements in each updated element from the AJAX response.
+     *
+     * @param {HTMLElement} updatedElement
+     */
     ajaxUpdate(updatedElement) {
+        this.initialisePlugins(updatedElement);
+    }
+
+    /**
+     * Initialises plugins within a given root element.
+     *
+     * Each plugin will receive one or more elements that match the element matches provided in the
+     * `elementMatchers()` method.
+     *
+     * @param {HTMLElement} rootElement
+     */
+    initialisePlugins(rootElement) {
         Object.entries(this.elementMatchers()).forEach(([plugin, selector]) => {
-            updatedElement.querySelectorAll(selector).forEach((element) => {
+            rootElement.querySelectorAll(selector).forEach((element) => {
                 this.snowboard[plugin](element);
             });
         });
