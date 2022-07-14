5443d0c9544db48c59830eb827429ba1dacc09a2
Ben Thomson <git@alfreido.com>
2022-07-14 16:09:21 +0800

    Recompile backend

--- modules/backend/assets/ui/js/index.js
+++ modules/backend/assets/ui/js/index.js
@@ -1,14 +1,27 @@
 import BackendAjaxHandler from './ajax/Handler';
 import BackendUIHandler from './UIHandler';
+import BalloonSelector from './controls/BalloonSelector';
 import DateTimeControl from './controls/DateTime';
+import DisposableObserver from './utilities/DisposableObserver';
+import Disposable from './utilities/Disposable';
 
 if (window.Snowboard === undefined) {
     throw new Error('Snowboard must be loaded in order to use the Backend UI.');
 }
 
 ((Snowboard) => {
+    // Utilities
+    Snowboard.addPlugin('disposableObserver', DisposableObserver);
+    Snowboard.addPlugin('disposable', Disposable);
+
+    // AJAX plugins
     Snowboard.addPlugin('backend.ajax.handler', BackendAjaxHandler);
+
+    // Controls
+    Snowboard.addPlugin('backend.controls.balloonSelector', BalloonSelector);
     Snowboard.addPlugin('backend.controls.dateTime', DateTimeControl);
+
+    // Main classes
     Snowboard.addPlugin('backend.ui.handler', BackendUIHandler);
 
     // Add the pre-filter immediately
