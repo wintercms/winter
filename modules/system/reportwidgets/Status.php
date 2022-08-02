6b369f0daef67ae84e3b3731d5c4295167c76205
Luke Towers <github@luketowers.ca>
2022-08-02 15:48:11 -0600

    Update modules/system/reportwidgets/Status.php

--- modules/system/reportwidgets/Status.php
+++ modules/system/reportwidgets/Status.php
@@ -70,12 +70,7 @@ class Status extends ReportWidgetBase
         $this->vars['requestLogMsg'] = LogSetting::get('log_requests', false) ? false : true;
 
         // TODO: Store system boot date in `Parameter`
-        $this->vars['appBirthday'] = null;
-        $appBirthday = PluginVersion::orderBy('created_at')->first();
-
-        if ($appBirthday) {
-            $this->vars['appBirthday'] = $appBirthday->created_at;
-        }
+        $this->vars['appBirthday'] = PluginVersion::orderBy('created_at')->first()?->created_at;
     }
 
     public function onLoadWarningsForm()
