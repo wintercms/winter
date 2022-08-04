09aad658b5f03effd734dbfede84a98ecb6d7d73
Arvis Lācis <arvis.lacis@gmail.com>
2022-08-04 22:56:22 +0300

    Fix status widget output on clean installation (#633)

--- modules/system/reportwidgets/Status.php
+++ modules/system/reportwidgets/Status.php
@@ -70,7 +70,7 @@ class Status extends ReportWidgetBase
         $this->vars['requestLogMsg'] = LogSetting::get('log_requests', false) ? false : true;
 
         // TODO: Store system boot date in `Parameter`
-        $this->vars['appBirthday'] = PluginVersion::orderBy('created_at')->first()->created_at;
+        $this->vars['appBirthday'] = PluginVersion::orderBy('created_at')->first()?->created_at;
     }
 
     public function onLoadWarningsForm()
