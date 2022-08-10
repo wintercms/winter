fbd650308458dfe87dde50af3b8033c11f3660b6
Jack Wilkinson <me@jackwilky.com>
2022-08-10 12:24:38 +0100

    Switched failing fixture to b

--- modules/system/tests/fixtures/plugins/mix/testb/assets/src/app.js
+++ modules/system/tests/fixtures/plugins/mix/testb/assets/src/app.js
@@ -1,8 +1,10 @@
-/* eslint-disable no-plusplus, no-console */
+/* eslint-disable no-plusplus, no-console, import/no-unresolved */
+const missing = require('some-missing-package');
+
 (() => {
     for (let i = 0; i <= 100; i++) {
         if ([2, 3].every((p) => i % p)) {
-            console.log(i);
+            missing.log(i);
         }
     }
 })();
--- modules/system/tests/fixtures/plugins/mix/testc/assets/src/app.js
+++ modules/system/tests/fixtures/plugins/mix/testc/assets/src/app.js
@@ -1,10 +1,8 @@
-/* eslint-disable no-plusplus, no-console, import/no-unresolved */
-const missing = require('some-missing-package');
-
+/* eslint-disable no-plusplus, no-console */
 (() => {
     for (let i = 0; i <= 100; i++) {
         if ([2, 3].every((p) => i % p)) {
-            missing.log(i);
+            console.log(i);
         }
     }
 })();
