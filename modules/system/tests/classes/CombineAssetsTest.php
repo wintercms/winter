13dab1c58eaef6079e48613ec67caf72740678c6
Romain 'Maz' BILLOIR <contact@maz.digital>
2022-08-11 11:54:37 +0200

    Add more aliases resolving tests

--- modules/system/tests/classes/CombineAssetsTest.php
+++ modules/system/tests/classes/CombineAssetsTest.php
@@ -107,25 +107,125 @@ class CombineAssetsTest extends TestCase
 
     public function testAliasesResolving()
     {
+        // Few temporary helpers to avoid typos
+        $assertContainsStyle1 = $assertContainsFrameworkCss =
+            fn ($response) => $this->assertStringContainsString('.style-1', $response->getContent());
+        $assertContainsScript1 = $assertContainsFrameworkJs =
+            fn ($response) => $this->assertStringContainsString('script1.js', $response->getContent());
+        $assertContainsStyle2 = fn ($response) => $this->assertStringContainsString('.style-2', $response->getContent());
+        $assertContainsScript2 = fn ($response) => $this->assertStringContainsString('script2.js', $response->getContent());
+
         $combiner = CombineAssets::instance();
+
+        // Clear the system registered aliases
+        $combiner->resetAliases();
 
         $combiner->registerAlias('css1', '~/modules/system/tests/fixtures/themes/test/assets/css/style1.css');
         $combiner->registerAlias('css2', '~/modules/system/tests/fixtures/themes/test/assets/css/style2.css');
+        $combiner->registerAlias('framework', '~/modules/system/tests/fixtures/themes/test/assets/css/style1.css');
         $combiner->registerAlias('js1', '~/modules/system/tests/fixtures/themes/test/assets/js/script1.js');
         $combiner->registerAlias('js2', '~/modules/system/tests/fixtures/themes/test/assets/js/script2.js');
+        $combiner->registerAlias('framework', '~/modules/system/tests/fixtures/themes/test/assets/js/script1.js');
 
+        // 0 aliases
         $url = $combiner->combine(
-            ['@js1', '@js2'],
+            ['assets/css/style1.css'],
             base_path() . '/modules/system/tests/fixtures/themes/test'
         );
         $this->assertNotNull($url);
-        $this->assertRegExp('/\w+[-]\d+/i', $url); // Must contain hash-number
+        $response = $this->get($url);
+        $assertContainsStyle1($response);
 
+        // 1 css
         $url = $combiner->combine(
-            ['@css1', '@css2'],
+            ['@css1'],
             base_path() . '/modules/system/tests/fixtures/themes/test'
         );
         $this->assertNotNull($url);
-        $this->assertRegExp('/\w+[-]\d+/i', $url); // Must contain hash-number
+        $response = $this->get($url);
+        $assertContainsStyle1($response);
+
+        // 1 js
+        $url = $combiner->combine(
+            ['@js1'],
+            base_path() . '/modules/system/tests/fixtures/themes/test'
+        );
+        $this->assertNotNull($url);
+        $response = $this->get($url);
+        $assertContainsScript1($response);
+
+        // 2 css 1 js
+        $url = $combiner->combine(
+            ['@css1', '@css2', '@js1'],
+            base_path() . '/modules/system/tests/fixtures/themes/test'
+        );
+        $this->assertNotNull($url);
+        $response = $this->get($url);
+        $assertContainsStyle1($response);
+        $assertContainsStyle2($response);
+        $assertContainsScript1($response);
+
+        // 1 css 2 js
+        $url = $combiner->combine(
+            ['@css1', '@js1', '@js2'],
+            base_path() . '/modules/system/tests/fixtures/themes/test'
+        );
+        $this->assertNotNull($url);
+        $response = $this->get($url);
+        $assertContainsStyle1($response);
+        $assertContainsScript1($response);
+        $assertContainsScript2($response);
+
+        // 2 css 2 js
+        $url = $combiner->combine(
+            ['@css1', '@css2', '@js1', '@js2'],
+            base_path() . '/modules/system/tests/fixtures/themes/test'
+        );
+        $this->assertNotNull($url);
+        $response = $this->get($url);
+        $assertContainsStyle1($response);
+        $assertContainsStyle2($response);
+        $assertContainsScript1($response);
+        $assertContainsScript2($response);
+
+        // 1 combined
+        $url = $combiner->combine(
+            ['@framework'],
+            base_path() . '/modules/system/tests/fixtures/themes/test'
+        );
+        $this->assertNotNull($url);
+        $response = $this->get($url);
+        $assertContainsFrameworkCss($response);
+
+        // 1 combined 1 css
+        $url = $combiner->combine(
+            ['@framework', '@css2'],
+            base_path() . '/modules/system/tests/fixtures/themes/test'
+        );
+        $this->assertNotNull($url);
+        $response = $this->get($url);
+        $assertContainsFrameworkCss($response);
+        $assertContainsStyle2($response);
+
+        // 1 combined 1 js
+        $url = $combiner->combine(
+            ['@framework', '@js2'],
+            base_path() . '/modules/system/tests/fixtures/themes/test'
+        );
+        $this->assertNotNull($url);
+        $response = $this->get($url);
+        $assertContainsFrameworkJs($response);
+        $assertContainsScript2($response);
+
+        // 1 combined 1 js 1 css
+        $url = $combiner->combine(
+            ['@framework', '@js2', '@css2'],
+            base_path() . '/modules/system/tests/fixtures/themes/test'
+        );
+        $this->assertNotNull($url);
+        $response = $this->get($url);
+        $assertContainsFrameworkCss($response);
+        $assertContainsScript2($response);
+        $assertContainsStyle2($response);
     }
 }
