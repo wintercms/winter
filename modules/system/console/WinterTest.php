10d1a32473effa07ecbeaf28a8d44732ac558863
Jack Wilkinson <31214002+jaxwilko@users.noreply.github.com>
2022-08-30 16:43:23 +0100

    Add support for overriding phpunit bootstrap

--- modules/system/console/WinterTest.php
+++ modules/system/console/WinterTest.php
@@ -141,6 +141,13 @@ class WinterTest extends Command
         // Resolve the configuration path based on the current working directory
         $config = realpath($config);
         $bootstrapPath = base_path('modules/system/tests/bootstrap/app.php');
+        
+        if (
+            ($configBoostrap = (string) simplexml_load_file($config)['bootstrap'])
+            && str_starts_with($configBoostrap, './')
+        ) {
+            $bootstrapPath = dirname($config) . ltrim($configBoostrap, '.');
+        }
 
         $process = new Process(
             array_merge([$this->phpUnitExec, '--configuration=' . $config, '--bootstrap=' . $bootstrapPath], $args),
