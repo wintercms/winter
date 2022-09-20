b35b67d1d0d37cee9e3545845604716f685cb113
Marc Jauvin <marc.jauvin@gmail.com>
2022-09-20 08:28:23 -0400

    fix column preview on Import

--- modules/backend/behaviors/ImportExportController.php
+++ modules/backend/behaviors/ImportExportController.php
@@ -12,9 +12,11 @@ use Illuminate\Database\Eloquent\MassAssignmentException;
 use League\Csv\Reader as CsvReader;
 use League\Csv\Writer as CsvWriter;
 use League\Csv\EscapeFormula as CsvEscapeFormula;
+use League\Csv\Statement as CsvStatement;
 use ApplicationException;
 use SplTempFileObject;
 use Exception;
+
 
 /**
  * Adds features for importing and exporting data.
@@ -250,10 +252,14 @@ class ImportExportController extends ControllerBehavior
         $reader = $this->createCsvReader($path);
 
         if (post('first_row_titles')) {
-            $reader->setOffset(1);
+            $reader->setHeaderOffset(1);
         }
 
-        $result = $reader->setLimit(50)->fetchColumn((int) $columnId);
+        $result = (new CsvStatement())
+            ￼            ->limit(50)
+            ￼            ->process($reader)
+            ￼            ->fetchColumn((int) $columnId);
+        ￼
         $data = iterator_to_array($result, false);
 
         /*
