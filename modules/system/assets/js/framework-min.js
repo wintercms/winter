wintercms/winter

commit 9637b67c239c1e9a5d4a3057beb3062fb5a56069

Correctly handle arrayed POST data in AJAX calls (#689)
Uses FormData object to handle arrayed POST data.

Fixes https://github.com/wintercms/winter/issues/683.

Refs:
- https://github.com/wintercms/winter/issues/683#issuecomment-1244682887 (also fixed)
- https://github.com/wintercms/winter/commit/a4cfd0d23f4342f530dc4b425098ee24086cd8d1

modules/system/assets/js/framework-min.js
@@ -52,7 +52,7 @@ $(window).trigger(_event,[fieldElement.get(0),fieldName,fieldMessages,isFirstInv
 if(isFirstInvalidField){if(!_event.isDefaultPrevented())fieldElement.focus()
 isFirstInvalidField=false}}})},handleFlashMessage:function(message,type){},handleRedirectResponse:function(url){$(window).one('popstate',function(){$el.trigger('ajaxRedirected')})
 window.location.assign(url)},handleUpdateResponse:function(data,textStatus,jqXHR){var updatePromise=$.Deferred().done(function(){for(var partial in data){var selector=(options.update[partial])?options.update[partial]:partial
-if($.type(selector)=='string'&&selector.charAt(0)=='@'){$(selector.substring(1)).append(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}else if($.type(selector)=='string'&&selector.charAt(0)=='^'){$(selector.substring(1)).prepend(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}else{$(selector).trigger('ajaxBeforeReplace')
+if($.type(selector)=='string'&&selector.charAt(0)=='@'){$(selector.substring(1)).append(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}else if($.type(selector)=='string'&&selector.charAt(0)=='^'){$(selector.substring(1)).prepend(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}else if($.type(selector)=='string'&&(selector.charAt(0)=='#'||selector.charAt(0)=='.')){$(selector).trigger('ajaxBeforeReplace')
 $(selector).html(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}}setTimeout(function(){$(window).trigger('ajaxUpdateComplete',[context,data,textStatus,jqXHR]).trigger('resize')},0)})
 if(data['X_WINTER_REDIRECT']){options.redirect=data['X_WINTER_REDIRECT']
 isRedirect=true}if(isRedirect){requestOptions.handleRedirectResponse(options.redirect)}if(data['X_WINTER_ERROR_FIELDS']){requestOptions.handleValidationMessage(data['X_WINTER_ERROR_MESSAGE'],data['X_WINTER_ERROR_FIELDS'])}if(data['X_WINTER_ASSETS']){assetManager.load(data['X_WINTER_ASSETS'],$.proxy(updatePromise.resolve,updatePromise))}else{updatePromise.resolve()}return updatePromise}}
@@ -79,10 +79,10 @@ $.fn.request.Constructor=Request
 $.request=function(handler,option){return $(document).request(handler,option)}
 $.fn.request.noConflict=function(){$.fn.request=old
 return this}
-$.fn.getRequestParentData=function(){var $form=$(this).first(),parentDataObjects=[serializeFormToObj($form)],parentFormData={};var findParentForms=function($form){if($form.length&&$form.data('request-parent')){var $parentEl=$($form.data('request-parent'));if($parentEl.length){var parentEmbeddedData={};$.each($parentEl.parents('[data-request-data]').toArray().reverse(),function extendRequest(){$.extend(parentEmbeddedData,paramToObj('data-request-data',$(this).data('request-data')));});if($parentEl.is('[data-request-data]')){$.extend(parentEmbeddedData,paramToObj('data-request-data',$parentEl.data('request-data')));}var $parentForm=$parentEl.closest('form');if($parentForm.length){parentDataObjects.push($.extend(serializeFormToObj($parentForm),parentEmbeddedData));findParentForms($parentForm);}}}};findParentForms($form);parentDataObjects.reverse().forEach(function(data){$.extend(parentFormData,data);});return parentFormData;}
+$.fn.getRequestParentData=function(){var $form=$(this).first(),parentDataObjects=[formDataToObj(new FormData($form.get(0)))],parentFormData={};var findParentForms=function($form){if($form.length&&$form.data('request-parent')){var $parentEl=$($form.data('request-parent'));if($parentEl.length){var parentEmbeddedData={};$.each($parentEl.parents('[data-request-data]').toArray().reverse(),function extendRequest(){$.extend(parentEmbeddedData,paramToObj('data-request-data',$(this).data('request-data')));});if($parentEl.is('[data-request-data]')){$.extend(parentEmbeddedData,paramToObj('data-request-data',$parentEl.data('request-data')));}var $parentForm=$parentEl.closest('form');if($parentForm.length){parentDataObjects.push($.extend(formDataToObj(new FormData($form.get(0))),parentEmbeddedData));findParentForms($parentForm);}}}};findParentForms($form);parentDataObjects.reverse().forEach(function(data){$.extend(parentFormData,data);});return parentFormData;}
 function paramToObj(name,value){if(value===undefined)value=''
 if(typeof value=='object')return value
-try{return ocJSON("{"+value+"}")}catch(e){throw new Error('Error parsing the '+name+' attribute value. '+e)}}function serializeFormToObj($form){var arrayData=$form.serializeArray(),objectData={};arrayData.forEach(function(field){objectData[field.name]=field.value;});return objectData;}function getXSRFToken(){var cookieValue=null
+try{return ocJSON("{"+value+"}")}catch(e){throw new Error('Error parsing the '+name+' attribute value. '+e)}}function formDataToObj(formDataInstance){var objectData={};for(const pair of formDataInstance.entries()){const key=pair[0];const value=pair[1];if(!Reflect.has(objectData,key)){objectData[key]=value;continue;}if(!Array.isArray(objectData[key])){objectData[key]=[objectData[key]];}objectData[key].push(value);}return objectData;}function getXSRFToken(){var cookieValue=null
 if(document.cookie&&document.cookie!=''){var cookies=document.cookie.split(';')
 for(var i=0;i<cookies.length;i++){var cookie=jQuery.trim(cookies[i])
 if(cookie.substring(0,11)==('XSRF-TOKEN'+'=')){cookieValue=decodeURIComponent(cookie.substring(11))

modules/system/assets/js/framework.combined-min.js
@@ -52,7 +52,7 @@ $(window).trigger(_event,[fieldElement.get(0),fieldName,fieldMessages,isFirstInv
 if(isFirstInvalidField){if(!_event.isDefaultPrevented())fieldElement.focus()
 isFirstInvalidField=false}}})},handleFlashMessage:function(message,type){},handleRedirectResponse:function(url){$(window).one('popstate',function(){$el.trigger('ajaxRedirected')})
 window.location.assign(url)},handleUpdateResponse:function(data,textStatus,jqXHR){var updatePromise=$.Deferred().done(function(){for(var partial in data){var selector=(options.update[partial])?options.update[partial]:partial
-if($.type(selector)=='string'&&selector.charAt(0)=='@'){$(selector.substring(1)).append(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}else if($.type(selector)=='string'&&selector.charAt(0)=='^'){$(selector.substring(1)).prepend(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}else{$(selector).trigger('ajaxBeforeReplace')
+if($.type(selector)=='string'&&selector.charAt(0)=='@'){$(selector.substring(1)).append(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}else if($.type(selector)=='string'&&selector.charAt(0)=='^'){$(selector.substring(1)).prepend(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}else if($.type(selector)=='string'&&(selector.charAt(0)=='#'||selector.charAt(0)=='.')){$(selector).trigger('ajaxBeforeReplace')
 $(selector).html(data[partial]).trigger('ajaxUpdate',[context,data,textStatus,jqXHR])}}setTimeout(function(){$(window).trigger('ajaxUpdateComplete',[context,data,textStatus,jqXHR]).trigger('resize')},0)})
 if(data['X_WINTER_REDIRECT']){options.redirect=data['X_WINTER_REDIRECT']
 isRedirect=true}if(isRedirect){requestOptions.handleRedirectResponse(options.redirect)}if(data['X_WINTER_ERROR_FIELDS']){requestOptions.handleValidationMessage(data['X_WINTER_ERROR_MESSAGE'],data['X_WINTER_ERROR_FIELDS'])}if(data['X_WINTER_ASSETS']){assetManager.load(data['X_WINTER_ASSETS'],$.proxy(updatePromise.resolve,updatePromise))}else{updatePromise.resolve()}return updatePromise}}
@@ -79,10 +79,10 @@ $.fn.request.Constructor=Request
 $.request=function(handler,option){return $(document).request(handler,option)}
 $.fn.request.noConflict=function(){$.fn.request=old
 return this}
-$.fn.getRequestParentData=function(){var $form=$(this).first(),parentDataObjects=[serializeFormToObj($form)],parentFormData={};var findParentForms=function($form){if($form.length&&$form.data('request-parent')){var $parentEl=$($form.data('request-parent'));if($parentEl.length){var parentEmbeddedData={};$.each($parentEl.parents('[data-request-data]').toArray().reverse(),function extendRequest(){$.extend(parentEmbeddedData,paramToObj('data-request-data',$(this).data('request-data')));});if($parentEl.is('[data-request-data]')){$.extend(parentEmbeddedData,paramToObj('data-request-data',$parentEl.data('request-data')));}var $parentForm=$parentEl.closest('form');if($parentForm.length){parentDataObjects.push($.extend(serializeFormToObj($parentForm),parentEmbeddedData));findParentForms($parentForm);}}}};findParentForms($form);parentDataObjects.reverse().forEach(function(data){$.extend(parentFormData,data);});return parentFormData;}
+$.fn.getRequestParentData=function(){var $form=$(this).first(),parentDataObjects=[formDataToObj(new FormData($form.get(0)))],parentFormData={};var findParentForms=function($form){if($form.length&&$form.data('request-parent')){var $parentEl=$($form.data('request-parent'));if($parentEl.length){var parentEmbeddedData={};$.each($parentEl.parents('[data-request-data]').toArray().reverse(),function extendRequest(){$.extend(parentEmbeddedData,paramToObj('data-request-data',$(this).data('request-data')));});if($parentEl.is('[data-request-data]')){$.extend(parentEmbeddedData,paramToObj('data-request-data',$parentEl.data('request-data')));}var $parentForm=$parentEl.closest('form');if($parentForm.length){parentDataObjects.push($.extend(formDataToObj(new FormData($form.get(0))),parentEmbeddedData));findParentForms($parentForm);}}}};findParentForms($form);parentDataObjects.reverse().forEach(function(data){$.extend(parentFormData,data);});return parentFormData;}
 function paramToObj(name,value){if(value===undefined)value=''
 if(typeof value=='object')return value
-try{return ocJSON("{"+value+"}")}catch(e){throw new Error('Error parsing the '+name+' attribute value. '+e)}}function serializeFormToObj($form){var arrayData=$form.serializeArray(),objectData={};arrayData.forEach(function(field){objectData[field.name]=field.value;});return objectData;}function getXSRFToken(){var cookieValue=null
+try{return ocJSON("{"+value+"}")}catch(e){throw new Error('Error parsing the '+name+' attribute value. '+e)}}function formDataToObj(formDataInstance){var objectData={};for(const pair of formDataInstance.entries()){const key=pair[0];const value=pair[1];if(!Reflect.has(objectData,key)){objectData[key]=value;continue;}if(!Array.isArray(objectData[key])){objectData[key]=[objectData[key]];}objectData[key].push(value);}return objectData;}function getXSRFToken(){var cookieValue=null
 if(document.cookie&&document.cookie!=''){var cookies=document.cookie.split(';')
 for(var i=0;i<cookies.length;i++){var cookie=jQuery.trim(cookies[i])
 if(cookie.substring(0,11)==('XSRF-TOKEN'+'=')){cookieValue=decodeURIComponent(cookie.substring(11))

modules/system/assets/js/framework.js
@@ -531,7 +531,7 @@ if (window.jQuery.request !== undefined) {
      */
     $.fn.getRequestParentData = function () {
         var $form = $(this).first(),
-            parentDataObjects = [serializeFormToObj($form)],
+            parentDataObjects = [formDataToObj(new FormData($form.get(0)))],
             parentFormData = {};
 
         var findParentForms = function ($form) {
@@ -557,7 +557,7 @@ if (window.jQuery.request !== undefined) {
                     if ($parentForm.length) {
                         // Add the identified parent form to the array for processing its data
                         // and check it for a parent element & containing form of its own
-                        parentDataObjects.push($.extend(serializeFormToObj($parentForm), parentEmbeddedData));
+                        parentDataObjects.push($.extend(formDataToObj(new FormData($form.get(0))), parentEmbeddedData));
                         findParentForms($parentForm);
                     }
                 }
@@ -592,13 +592,22 @@ if (window.jQuery.request !== undefined) {
         }
     }
 
-    function serializeFormToObj($form) {
-        var arrayData = $form.serializeArray(),
-            objectData = {};
+    function formDataToObj(formDataInstance) {
+        var objectData = {};
 
-        arrayData.forEach(function (field) {
-            objectData[field.name] = field.value;
-        });
+        for (const pair of formDataInstance.entries()) {
+            const key = pair[0];
+            const value = pair[1];
+
+            if (!Reflect.has(objectData, key)) {
+                objectData[key] = value;
+                continue;
+            }
+            if (!Array.isArray(objectData[key])) {
+                objectData[key] = [objectData[key]];
+            }
+            objectData[key].push(value);
+        }
 
         return objectData;
     }

modules/backend/formwidgets/richeditor/assets/js/build-plugins-min.js
@@ -146,7 +146,7 @@ this.$form.on('oc.beforeRequest',this.proxy(this.onFormBeforeRequest))
 this.$textarea.froalaEditor(froalaOptions)
 this.editor=this.$textarea.data('froala.editor')
 if(this.options.readOnly){this.editor.edit.off()}this.$el.on('keydown','.fr-view figure',this.proxy(this.onFigureKeydown))
-Snowboard.globalEvent("formwidgets.richeditor.init",this);}
+Snowboard.globalEvent("formwidgets.richeditor.init",this)}
 RichEditor.prototype.dispose=function(){this.unregisterHandlers()
 this.$textarea.froalaEditor('destroy')
 this.$el.removeData('oc.richEditor')

commit 08785a4a68d04c441655cb1df8a7f32309a2799e

Fix support for data-request-parent
Fixes bug introduced in https://github.com/wintercms/winter/commit/9637b67c239c1e9a5d4a3057beb3062fb5a56069 that broke support for data-request-parent.

modules/system/assets/js/framework-min.js
@@ -79,7 +79,7 @@ $.fn.request.Constructor=Request
 $.request=function(handler,option){return $(document).request(handler,option)}
 $.fn.request.noConflict=function(){$.fn.request=old
 return this}
-$.fn.getRequestParentData=function(){var $form=$(this).first(),parentDataObjects=[formDataToObj(new FormData($form.get(0)))],parentFormData={};var findParentForms=function($form){if($form.length&&$form.data('request-parent')){var $parentEl=$($form.data('request-parent'));if($parentEl.length){var parentEmbeddedData={};$.each($parentEl.parents('[data-request-data]').toArray().reverse(),function extendRequest(){$.extend(parentEmbeddedData,paramToObj('data-request-data',$(this).data('request-data')));});if($parentEl.is('[data-request-data]')){$.extend(parentEmbeddedData,paramToObj('data-request-data',$parentEl.data('request-data')));}var $parentForm=$parentEl.closest('form');if($parentForm.length){parentDataObjects.push($.extend(formDataToObj(new FormData($form.get(0))),parentEmbeddedData));findParentForms($parentForm);}}}};findParentForms($form);parentDataObjects.reverse().forEach(function(data){$.extend(parentFormData,data);});return parentFormData;}
+$.fn.getRequestParentData=function(){var $form=$(this).first(),parentDataObjects=[formDataToObj(new FormData($form.get(0)))],parentFormData={};var findParentForms=function($form){if($form.length&&$form.data('request-parent')){var $parentEl=$($form.data('request-parent'));if($parentEl.length){var parentEmbeddedData={};$.each($parentEl.parents('[data-request-data]').toArray().reverse(),function extendRequest(){$.extend(parentEmbeddedData,paramToObj('data-request-data',$(this).data('request-data')));});if($parentEl.is('[data-request-data]')){$.extend(parentEmbeddedData,paramToObj('data-request-data',$parentEl.data('request-data')));}var $parentForm=$parentEl.closest('form');if($parentForm.length){parentDataObjects.push($.extend(formDataToObj(new FormData($parentForm.get(0))),parentEmbeddedData));findParentForms($parentForm);}}}};findParentForms($form);parentDataObjects.reverse().forEach(function(data){$.extend(parentFormData,data);});return parentFormData;}
 function paramToObj(name,value){if(value===undefined)value=''
 if(typeof value=='object')return value
 try{return ocJSON("{"+value+"}")}catch(e){throw new Error('Error parsing the '+name+' attribute value. '+e)}}function formDataToObj(formDataInstance){var objectData={};for(const pair of formDataInstance.entries()){const key=pair[0];const value=pair[1];if(!Reflect.has(objectData,key)||!key.includes('[]')){objectData[key]=value;continue;}if(!Array.isArray(objectData[key])){objectData[key]=[objectData[key]];}objectData[key].push(value);}return objectData;}function getXSRFToken(){var cookieValue=null

modules/system/assets/js/framework.combined-min.js
@@ -79,7 +79,7 @@ $.fn.request.Constructor=Request
 $.request=function(handler,option){return $(document).request(handler,option)}
 $.fn.request.noConflict=function(){$.fn.request=old
 return this}
-$.fn.getRequestParentData=function(){var $form=$(this).first(),parentDataObjects=[formDataToObj(new FormData($form.get(0)))],parentFormData={};var findParentForms=function($form){if($form.length&&$form.data('request-parent')){var $parentEl=$($form.data('request-parent'));if($parentEl.length){var parentEmbeddedData={};$.each($parentEl.parents('[data-request-data]').toArray().reverse(),function extendRequest(){$.extend(parentEmbeddedData,paramToObj('data-request-data',$(this).data('request-data')));});if($parentEl.is('[data-request-data]')){$.extend(parentEmbeddedData,paramToObj('data-request-data',$parentEl.data('request-data')));}var $parentForm=$parentEl.closest('form');if($parentForm.length){parentDataObjects.push($.extend(formDataToObj(new FormData($form.get(0))),parentEmbeddedData));findParentForms($parentForm);}}}};findParentForms($form);parentDataObjects.reverse().forEach(function(data){$.extend(parentFormData,data);});return parentFormData;}
+$.fn.getRequestParentData=function(){var $form=$(this).first(),parentDataObjects=[formDataToObj(new FormData($form.get(0)))],parentFormData={};var findParentForms=function($form){if($form.length&&$form.data('request-parent')){var $parentEl=$($form.data('request-parent'));if($parentEl.length){var parentEmbeddedData={};$.each($parentEl.parents('[data-request-data]').toArray().reverse(),function extendRequest(){$.extend(parentEmbeddedData,paramToObj('data-request-data',$(this).data('request-data')));});if($parentEl.is('[data-request-data]')){$.extend(parentEmbeddedData,paramToObj('data-request-data',$parentEl.data('request-data')));}var $parentForm=$parentEl.closest('form');if($parentForm.length){parentDataObjects.push($.extend(formDataToObj(new FormData($parentForm.get(0))),parentEmbeddedData));findParentForms($parentForm);}}}};findParentForms($form);parentDataObjects.reverse().forEach(function(data){$.extend(parentFormData,data);});return parentFormData;}
 function paramToObj(name,value){if(value===undefined)value=''
 if(typeof value=='object')return value
 try{return ocJSON("{"+value+"}")}catch(e){throw new Error('Error parsing the '+name+' attribute value. '+e)}}function formDataToObj(formDataInstance){var objectData={};for(const pair of formDataInstance.entries()){const key=pair[0];const value=pair[1];if(!Reflect.has(objectData,key)||!key.includes('[]')){objectData[key]=value;continue;}if(!Array.isArray(objectData[key])){objectData[key]=[objectData[key]];}objectData[key].push(value);}return objectData;}function getXSRFToken(){var cookieValue=null

modules/system/assets/js/framework.js
@@ -557,7 +557,7 @@ if (window.jQuery.request !== undefined) {
                     if ($parentForm.length) {
                         // Add the identified parent form to the array for processing its data
                         // and check it for a parent element & containing form of its own
-                        parentDataObjects.push($.extend(formDataToObj(new FormData($form.get(0))), parentEmbeddedData));
+                        parentDataObjects.push($.extend(formDataToObj(new FormData($parentForm.get(0))), parentEmbeddedData));
                         findParentForms($parentForm);
                     }
                 }