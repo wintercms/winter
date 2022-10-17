2f3fdc64cee2504508b22f20c9fe94c0103c3eac
Eric Pfeiffer <epfeiffer@thebdx.com>
2022-10-17 10:30:26 -0500

    Change payload column to longtext to be consistent with Laravel's default migration stub.

---
+++ modules/system/database/migrations/2022_10_17_000028_Db_Jobs_Update_Payload_To_Longtext.php
@@ -0,0 +1,23 @@
+<?php
+
+use Winter\Storm\Database\Schema\Blueprint;
+
+return new class extends \Winter\Storm\Database\Updates\Migration
+{
+    public function up()
+    {
+        Schema::table($this->getTableName(), function (Blueprint $table) {
+            $table->longText('payload')->change();
+        });
+    }
+
+    public function down()
+    {
+
+    }
+
+    protected function getTableName()
+    {
+        return Config::get('queue.connections.database.table', 'jobs');
+    }
+};
