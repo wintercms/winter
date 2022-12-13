5e34057473ad2a20b255b2f4c27f0f55ea4b998d
Ben Thomson <git@alfreido.com>
2022-12-13 09:58:57 +0800

    Initial setup of Monaco build process

--- modules/backend/winter.mix.js
+++ modules/backend/winter.mix.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
 const mix = require('laravel-mix');
+const path = require('path');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 require('laravel-mix-polyfill');
 /* eslint-enable */
 
@@ -23,6 +25,62 @@ mix
         './formwidgets/iconpicker/assets/dist/iconpicker.js',
     )
     .vue()
+
+    // Compile widgets for Backend
+    .js(
+        './formwidgets/codeeditor/assets/js/codeeditor.js',
+        './formwidgets/codeeditor/assets/js/build/codeeditor.bundle.js',
+    )
+    .webpackConfig({
+        plugins: [
+            new MonacoWebpackPlugin({
+                filename: './formwidgets/codeeditor/assets/js/build/[name].worker.js',
+                publicPath: path.resolve(__dirname, './formwidgets/codeeditor/assets/js/build/'),
+                languages: [
+                    'typescript',
+                    'javascript',
+                    'css',
+                    'json',
+                    'html',
+                    'ini',
+                    'less',
+                    'markdown',
+                    'mysql',
+                    'php',
+                    'scss',
+                    'twig',
+                    'xml',
+                    'yaml',
+                ],
+                features: [
+                    'anchorSelect',
+                    'bracketMatching',
+                    'caretOperations',
+                    'clipboard',
+                    'colorPicker',
+                    'comment',
+                    'contextmenu',
+                    'cursorUndo',
+                    'find',
+                    'folding',
+                    'gotoSymbol',
+                    'hover',
+                    'inPlaceReplace',
+                    'indentation',
+                    'inlineHints',
+                    'links',
+                    'multicursor',
+                    'parameterHints',
+                    'rename',
+                    'smartSelect',
+                    'snippet',
+                    'suggest',
+                    'wordHighlighter',
+                    'wordOperations',
+                ],
+            }),
+        ],
+    })
 
     // Polyfill for all targeted browsers
     .polyfill({
