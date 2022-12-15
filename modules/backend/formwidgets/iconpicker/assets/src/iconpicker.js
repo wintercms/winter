3e2b855a2428be295f3a9abc8ac487b965b58e39
Jack Wilkinson <me@jackwilky.com>
2022-12-15 17:13:41 +0000

    Fixed issue with iconpicker not being registered after load and added support for providing the form during the snowboard request

--- modules/backend/formwidgets/iconpicker/assets/src/iconpicker.js
+++ modules/backend/formwidgets/iconpicker/assets/src/iconpicker.js
@@ -1,20 +1,28 @@
 import { createApp } from 'vue'
 import IconPicker from './components/IconPicker.vue'
 
-window.addEventListener('load', () => {
-    const elements = document.querySelectorAll("[data-control='iconpicker']:not([data-v-app])");
-    elements.forEach(element => {
-        element.querySelector(".input-group").addEventListener("click", () => {
-            snowboard.request(null, element.dataset.alias + "::onLoadIconLibrary", {
-                success: (data) => {
-                    let iconPicker = createApp(IconPicker, {
-                        ...element.dataset,
-                        fontLibraries: JSON.parse(data.result)
-                    }).mount(element);
+(() => {
+    const init = () => {
+        const elements = document.querySelectorAll("[data-control='iconpicker']:not([data-v-app])");
+        elements.forEach(element => {
+            element.querySelector(".input-group").addEventListener("click", () => {
+                snowboard.request(element.querySelector("input")?.form || null, element.dataset.alias + "::onLoadIconLibrary", {
+                    success: (data) => {
+                        let iconPicker = createApp(IconPicker, {
+                            ...element.dataset,
+                            fontLibraries: JSON.parse(data.result)
+                        }).mount(element);
 
-                    iconPicker.togglePicker();
-                },
-            })
+                        iconPicker.togglePicker();
+                    },
+                })
+            });
         });
-    });
-});
+    };
+
+    window.addEventListener('load', init);
+
+    if (document.readyState === "complete") {
+        init();
+    }
+})();
