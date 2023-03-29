ece4074b7c15db113d6f70734aa6931a5f0fc0f9
AIC BV <89913092+AIC-BV@users.noreply.github.com>
2023-03-29 09:31:15 +0200

    Remove left-over debug info

--- modules/cms/models/ThemeLog.php
+++ modules/cms/models/ThemeLog.php
@@ -77,12 +77,6 @@ class ThemeLog extends Model
         $newContent = $template->toCompiled();
         $oldContent = $template->getOriginal('content');
 
-        if ($newContent === $oldContent && $templateName === $oldTemplateName && !$isDelete) {
-            traceLog($newContent, $oldContent);
-            traceLog('Content not dirty for: '. $template->getObjectTypeDirName().'/'.$template->fileName);
-            return;
-        }
-
         $record = new self;
         $record->type = $type;
         $record->theme = Theme::getEditThemeCode();
