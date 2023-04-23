fb8072750101c12b4e931efde10e87c2a5b3c134
Zach Schneider <hello@zacharyschneider.ca>
2023-04-23 07:53:49 -0700

    Add sizeBytes & lastModified to mediamanager.js getItems() (#890)
    This adds the sizeBytes & lastModified item attributes to the getItems() function in mediamanager.js exposing them for use in the onInsert() handler of the MediaManager popup.
    
    Credit to @zaxbux

--- modules/backend/widgets/mediamanager/assets/js/mediamanager.js
+++ modules/backend/widgets/mediamanager/assets/js/mediamanager.js
@@ -98,6 +98,8 @@
                     itemType: item.getAttribute('data-item-type'),
                     path: item.getAttribute('data-path'),
                     title: item.getAttribute('data-title'),
+                    sizeBytes: item.getAttribute('data-size-bytes'),
+                    lastModified: item.getAttribute('data-last-modified-ts'),
                     documentType: item.getAttribute('data-document-type'),
                     folder: item.getAttribute('data-folder'),
                     publicUrl: item.getAttribute('data-public-url')
