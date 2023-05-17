ebacfb59511be5b9d4aaa9d6886cce5c055e1bc4
Marc Jauvin <marc.jauvin@gmail.com>
2023-05-17 08:52:55 -0400

    add type hidden removed by mistake

--- modules/backend/widgets/form/partials/_field_dropdown.php
+++ modules/backend/widgets/form/partials/_field_dropdown.php
@@ -9,7 +9,7 @@ $emptyOption = $field->getConfig('emptyOption', $field->placeholder);
     <div class="form-control" <?= $field->readOnly ? 'disabled="disabled"' : ''; ?>>
         <?= (isset($fieldOptions[$field->value])) ? e(trans($fieldOptions[$field->value])) : '' ?>
     </div>
-    <input name="<?= $field->getName() ?>" value="<?= $field->value ?>">
+    <input type="hidden" name="<?= $field->getName() ?>" value="<?= $field->value ?>">
 <?php else: ?>
     <select
         id="<?= $field->getId() ?>"
