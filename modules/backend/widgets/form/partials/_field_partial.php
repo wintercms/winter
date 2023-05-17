07412ed35c7bcd8bfce71e9adcd3c19582d6d63f
Luke Towers <luke@luketowers.ca>
2023-05-17 15:13:07 -0600

    Include formWidget in the variables provided to partial fields

--- modules/backend/widgets/form/partials/_field_partial.php
+++ modules/backend/widgets/form/partials/_field_partial.php
@@ -1,9 +1,9 @@
 <?= $this->controller->makePartial($field->path ?: $field->fieldName, [
-    'formModel' => $formModel,
-    'formField' => $field,
-    'formValue' => $field->value,
-    'model'     => $formModel,
-    'field'     => $field,
-    'value'     => $field->value,
-    'previewMode' => $this->previewMode,
+    'formWidget' => $this,
+    'formModel'  => $formModel,
+    'formField'  => $field,
+    'formValue'  => $field->value,
+    'model'      => $formModel,
+    'field'      => $field,
+    'value'      => $field->value
 ]) ?>
