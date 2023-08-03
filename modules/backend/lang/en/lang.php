249cdea1d561df3e76507bf2be795417b70e6707
AIC BV <89913092+AIC-BV@users.noreply.github.com>
2023-08-03 15:38:24 +0200

    Add dimensions to lang.php

--- modules/backend/lang/en/lang.php
+++ modules/backend/lang/en/lang.php
@@ -606,6 +606,7 @@ return [
         'filter_audio' => 'Audio',
         'filter_documents' => 'Documents',
         'library' => 'Library',
+        'dimensions' => 'Dimensions',
         'size' => 'Size',
         'title' => 'Title',
         'last_modified' => 'Last modified',
