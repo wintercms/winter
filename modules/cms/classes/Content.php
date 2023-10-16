c311471b17ebf2f28865db84e40cd12faba40655
Ben Thomson <git@alfreido.com>
2023-10-16 11:45:08 +0800

    Use temporary rewriting of curly brackets when rendering MD content
    Fixes https://github.com/wintercms/winter/issues/992

--- modules/cms/classes/Content.php
+++ modules/cms/classes/Content.php
@@ -1,7 +1,9 @@
-<?php namespace Cms\Classes;
+<?php
 
-use File;
-use Markdown;
+namespace Cms\Classes;
+
+use Winter\Storm\Support\Facades\File;
+use Winter\Storm\Support\Facades\Markdown;
 
 /**
  * The CMS content file class.
@@ -62,12 +64,46 @@ class Content extends CmsCompoundObject
                 $result = htmlspecialchars($this->markup);
                 break;
             case 'md':
-                $result = Markdown::parse($this->markup);
+                $result = $this->parseMarkdownMarkup($this->markup);
                 break;
             default:
                 $result = $this->markup;
         }
 
         return $result;
+    }
+
+    /**
+     * Parse Markdown content files.
+     *
+     * This method replaces curly variables in the content temporarily while Markdown rendering takes place, to
+     * circumvent the escaping that Commonmark does on curly brackets in links.
+     *
+     * @param string $markup
+     * @return string
+     */
+    protected function parseMarkdownMarkup(string $markup): string
+    {
+        /**
+         * Replace variables temporarily for Markdown parsing, as the Commonmark library escapes curly brackets in
+         * links.
+         * @see https://github.com/wintercms/winter/issues/992
+         */
+        $variables = [];
+        $markup = preg_replace_callback('/\{([^\n \}]+)\}/', function (array $matches) use (&$variables) {
+            $signature = hash('sha256', $matches[1]);
+            if (!array_key_exists($signature, $variables)) {
+                $variables[$signature] = $matches[1];
+            }
+            return ".=VAR={$signature}=.";
+        }, $markup);
+
+        $markup = Markdown::parse($markup);
+
+        foreach ($variables as $signature => $variable) {
+            $markup = str_replace(".=VAR={$signature}=.", "{{$variable}}", $markup);
+        }
+
+        return $markup;
     }
 }
