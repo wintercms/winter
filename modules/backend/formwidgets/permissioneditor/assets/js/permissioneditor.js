2d67d42ebcfbb6440a4d4a11266fe0ce661fe9d1
Marvin Durot <138058170+marvindurot@users.noreply.github.com>
2024-01-07 02:45:31 +0100

    Support mass enable / disable in PermissionEditor (#936)
    Co-authored-by: Ben Thomson <git@alfreido.com>
    Co-authored-by: Marvin Durot <138058170+durotm@users.noreply.github.com>

--- modules/backend/formwidgets/permissioneditor/assets/js/permissioneditor.js
+++ modules/backend/formwidgets/permissioneditor/assets/js/permissioneditor.js
@@ -13,6 +13,7 @@
     PermissionEditor.prototype.constructor = PermissionEditor
 
     PermissionEditor.prototype.init = function() {
+        $(document).on('click', '.permissioneditor table th.permission-type', this.proxy(this.onPermissionTypeClick))
         $(document).on('click', '.permissioneditor table td.permission-name', this.proxy(this.onPermissionNameClick))
         $(document).on('click', '.permissioneditor table tr.mode-checkbox input[type=checkbox]', this.proxy(this.onPermissionCheckboxClick))
         $(document).on('click', '.permissioneditor table tr.mode-radio input[type=radio]', this.proxy(this.onPermissionRadioClick))
@@ -20,6 +21,37 @@
 
     // EVENT HANDLERS
     // ============================
+
+    PermissionEditor.prototype.onPermissionTypeClick = function (ev) {
+        var $rows = $(ev.target).closest('tr').nextAll()
+        var index = $(ev.target).index()
+
+        var allChecked = true
+
+        for (let i = 0; i < $rows.length; i++) {
+            var $row = $rows.eq(i)
+            var $check = $row.find('td:nth-child(' + (index + 1) + ') input[type=radio], td:nth-child(' + (index + 1) + ') input[type=checkbox]').eq(0)
+
+            if ($check.length > 0) {
+                if (!$check[0].checked) {
+                    allChecked = false
+                    break
+                }
+            }
+        }
+
+        for (let i = 0; i < $rows.length; i++) {
+            var $row = $rows.eq(i)
+            var $check = $row.find('td:nth-child(' + (index + 1) + ') input[type=radio], td:nth-child(' + (index + 1) + ') input[type=checkbox]').eq(0)
+            if ($check.length > 0) {
+                if ($check.is('input[type=checkbox]')) {
+                    $check.prop('checked', !allChecked)
+                } else {
+                    $check.prop('checked', true)
+                }
+            }
+        }
+    };
 
     PermissionEditor.prototype.onPermissionNameClick = function(ev) {
         var $row = $(ev.target).closest('tr'),
