9c1d2a8af8e5c29c0e96e0df8a70bc2e062002df
Marc Jauvin <marc.jauvin@gmail.com>
2024-04-23 10:25:52 -0400

    The mail drivers now provide these by default

--- config/services.php
+++ config/services.php
@@ -13,18 +13,4 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
-
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'secret' => env('MAILGUN_SECRET'),
-    ],
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-    ],
 ];
