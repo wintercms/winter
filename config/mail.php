604fbf54c8b5368ceebeba8b7f4b9a64d2056fe4
Luke Towers <luke@luketowers.ca>
2024-04-25 17:42:21 -0600

    Reorder mail config keys to match Laravel

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
+            'port' => env('MAIL_PORT', 587),
             'timeout' => null,
             'transport' => 'smtp',
+            'username' => env('MAIL_USERNAME'),
         ],
         'ses' => [
             'transport' => 'ses',
