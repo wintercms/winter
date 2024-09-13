3100bbcffb02c874df38f59ca390f5a37c4a1808
Eric Pfeiffer <eric.pfeiffer@bluetangerine.com>
2024-09-12 20:35:07 -0500

    Fix code styling.

--- modules/system/classes/asset/Mix.php
+++ modules/system/classes/asset/Mix.php
@@ -3,6 +3,7 @@
 namespace System\Classes\Asset;
 
 use Cms\Classes\Theme;
+use Exception;
 use Illuminate\Support\HtmlString;
 use Winter\Storm\Support\Facades\Config;
 use Winter\Storm\Support\Facades\Url;
@@ -18,7 +19,7 @@ class Mix
 
         $path = Str::start($path, '/');
 
-        if ($manifestDirectory && ! str_starts_with($manifestDirectory, '/')) {
+        if ($manifestDirectory && !str_starts_with($manifestDirectory, '/')) {
             $manifestDirectory = "/$manifestDirectory";
         } else {
             $manifestDirectory = Str::start($theme->getConfigValue('mix_manifest_path', '/'), '/');
@@ -26,9 +27,9 @@ class Mix
 
         $manifestPath = $theme->getPath($theme->getDirName() . '/' . $manifestDirectory . '/mix-manifest.json');
 
-        if (! isset($manifests[$manifestPath])) {
-            if (! is_file($manifestPath)) {
-                throw new \Exception('The Mix manifest does not exist.');
+        if (!isset($manifests[$manifestPath])) {
+            if (!is_file($manifestPath)) {
+                throw new Exception('The Mix manifest does not exist.');
             }
 
             $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
@@ -36,10 +37,10 @@ class Mix
 
         $manifest = $manifests[$manifestPath];
 
-        if (! isset($manifest[$path])) {
-            $exception = new \Exception("Unable to locate Mix file: $path");
+        if (!isset($manifest[$path])) {
+            $exception = new Exception("Unable to locate Mix file: $path");
 
-            if (! app('config')->get('app.debug')) {
+            if (!app('config')->get('app.debug')) {
                 report($exception);
 
                 return $path;
