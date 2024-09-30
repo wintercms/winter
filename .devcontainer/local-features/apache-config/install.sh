b429fd63aa05a634cf339d05dc9ef574e3a11cd4
Ben Thomson <git@alfreido.com>
2024-09-30 05:41:19 +0000

    Only listen to port 8080 in Apache

--- .devcontainer/local-features/apache-config/install.sh
+++ .devcontainer/local-features/apache-config/install.sh
@@ -18,7 +18,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get -y install --no-install-recommends lynx
 usermod -aG www-data ${USERNAME}
-sed -i -e "s/Listen 80/Listen 80\\nListen 8080/g" /etc/apache2/ports.conf
+echo "Listen 8080" > /etc/apache2/ports.conf
 apt-get clean -y && rm -rf /var/lib/apt/lists/*
 
 echo "Done!"
