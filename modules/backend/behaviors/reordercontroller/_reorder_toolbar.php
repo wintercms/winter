2f616e0f8e9e31669b546b83af159c0e8cc51e00
Jack Wilkinson <me@jackwilky.com>
2025-02-18 17:43:26 +0000

    Added fixes for reorder partials

--- modules/backend/behaviors/reordercontroller/_reorder_toolbar.php
+++ modules/backend/behaviors/reordercontroller/_reorder_toolbar.php
@@ -1,11 +1,5 @@
-<?php
-$formController = $this->getClassExtension(\Backend\Behaviors\FormController::class);
-$formConfig = $formController?->getConfig();
-?>
-<?php if (!empty($formConfig)): ?>
-    <div data-control="toolbar">
-        <a href="<?= Backend::url($formConfig->defaultRedirect) ?>" class="btn btn-primary oc-icon-caret-left">
-            <?= e(trans('backend::lang.form.return_to_list')) ?>
-        </a>
-    </div>
-<?php endif; ?>
+<div data-control="toolbar">
+    <a href="<?= $this->actionUrl('/') ?>" class="btn btn-primary oc-icon-caret-left">
+        <?= e(trans('backend::lang.form.return_to_list')) ?>
+    </a>
+</div>
--- modules/backend/behaviors/reordercontroller/reorder.php
+++ modules/backend/behaviors/reordercontroller/reorder.php
@@ -1,13 +1,5 @@
-<?php
-$formController = $this->getClassExtension(\Backend\Behaviors\FormController::class);
-$formConfig = $formController?->getConfig();
-?>
-<?php if (!empty($formConfig)): ?>
-    <?php Block::put('breadcrumb') ?>
-        <ul>
-            <li><a href="<?= Backend::url($formConfig->defaultRedirect) ?>"><?= e(trans($formConfig->name . '_plural')); ?></a></li>
-            <li><?= e(trans('backend::lang.reorder.default_title')) ?></li>
-        </ul>
-    <?php Block::endPut() ?>
-<?php endif; ?>
+<?php Block::put('breadcrumb') ?>
+    <?= $this->makeLayoutPartial('breadcrumb') ?>
+<?php Block::endPut() ?>
+
 <?= $this->reorderRender() ?>
