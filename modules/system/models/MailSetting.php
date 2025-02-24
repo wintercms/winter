c4d403577bb80eefda528552c618debc5c486ee9
Marc Jauvin <marc.jauvin@gmail.com>
2025-02-24 11:28:57 -0500

    add validation rule for failover_mailers

--- modules/system/models/MailSetting.php
+++ modules/system/models/MailSetting.php
@@ -40,6 +40,7 @@ class MailSetting extends Model
      * Validation rules
      */
     public $rules = [
+        'failover_mailers' => 'required_if:send_mode,'.self::MODE_FAILOVER,
         'sender_name'  => 'required',
         'sender_email' => 'required|email'
     ];
