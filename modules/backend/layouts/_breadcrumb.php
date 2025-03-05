d6449e87723fd98475cf9df142a3509c2ef9bf6d
Luke Towers <git@luketowers.ca>
2025-03-05 15:02:03 -0600

    Improve breadcrumb generation logic to take into account settings contexts

--- modules/backend/layouts/_breadcrumb.php
+++ modules/backend/layouts/_breadcrumb.php
@@ -2,12 +2,20 @@
 $menu = \Backend\Facades\BackendMenu::getActiveMainMenuItem();
 $context = \Backend\Facades\BackendMenu::getContext();
 $sideMenu = $menu->sideMenu[$context->sideMenuCode ?? null] ?? null;
+$settingsManager = \System\Classes\SettingsManager::instance();
+$settingsContext = $settingsManager->getContext();
+$settingsItem = null;
+if ($settingsContext) {
+    $settingsItem = $settingsManager->findSettingItem($settingsContext->owner, $settingsContext->itemCode);
+}
 ?>
 <?php if ($menu): ?>
 <ul>
     <li><a href="<?= $menu->url ?>"><?= e(trans($menu->label)) ?></a></li>
     <?php if ($sideMenu): ?>
         <li><a href="<?= $sideMenu->url ?>"><?= e(trans($sideMenu->label)) ?></a></li>
+    <?php elseif ($settingsItem): ?>
+        <li><a href="<?= $settingsItem->url ?>"><?= e(trans($settingsItem->label)) ?></a></li>
     <?php endif; ?>
     <li><?= e(trans($this->pageTitle)) ?></li>
 </ul>
