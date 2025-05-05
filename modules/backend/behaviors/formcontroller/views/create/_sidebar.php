9a84505d762a958fa2350cc1d6f1de6457ec5221
Marc Jauvin <marc.jauvin@gmail.com>
2025-05-05 16:05:07 -0400

    extract create/update form buttons into their own partials

--- modules/backend/behaviors/formcontroller/views/create/_sidebar.php
+++ modules/backend/behaviors/formcontroller/views/create/_sidebar.php
@@ -11,32 +11,7 @@
             </div>
 
             <div class="form-buttons p-t">
-                <div class="loading-indicator-container">
-                    <button
-                        type="button"
-                        data-request="onSave"
-                        data-browser-validate
-                        data-hotkey="ctrl+s, cmd+s"
-                        data-load-indicator="<?= e(trans('backend::lang.form.creating_name', ['name' => trans($formConfig->name)])); ?>"
-                        data-request-before-update="$el.trigger('unchange.oc.changeMonitor')"
-                        class="btn btn-primary">
-                        <?= e(trans('backend::lang.form.create')); ?>
-                    </button>
-                    <button
-                        type="button"
-                        data-request="onSave"
-                        data-browser-validate
-                        data-request-data="close:1"
-                        data-hotkey="ctrl+enter, cmd+enter"
-                        data-load-indicator="<?= e(trans('backend::lang.form.creating_name', ['name' => trans($formConfig->name)])); ?>"
-                        data-request-before-update="$el.trigger('unchange.oc.changeMonitor')"
-                        class="btn btn-default">
-                        <?= e(trans('backend::lang.form.create_and_close')); ?>
-                    </button>
-                    <span class="btn-text">
-                        <?= e(trans('backend::lang.form.or')) ?> <a href="<?= Backend::url($formConfig->defaultRedirect) ?>"><?= e(trans('backend::lang.form.cancel')); ?></a>
-                    </span>
-                </div>
+                <?= $this->makePartial('form_create_toolbar', ['formConfig' => $formConfig]) ?>
             </div>
         </div>
     <?php Block::endPut() ?>
---
+++ modules/backend/behaviors/formcontroller/views/create/sidebar/_form_create_toolbar.php
@@ -0,0 +1,26 @@
+<div class="loading-indicator-container">
+    <button
+        type="button"
+        data-request="onSave"
+        data-browser-validate
+        data-hotkey="ctrl+s, cmd+s"
+        data-load-indicator="<?= e(trans('backend::lang.form.creating_name', ['name' => trans($formConfig->name)])); ?>"
+        data-request-before-update="$el.trigger('unchange.oc.changeMonitor')"
+        class="btn btn-primary">
+        <?= e(trans('backend::lang.form.create')); ?>
+    </button>
+    <button
+        type="button"
+        data-request="onSave"
+        data-browser-validate
+        data-request-data="close:1"
+        data-hotkey="ctrl+enter, cmd+enter"
+        data-load-indicator="<?= e(trans('backend::lang.form.creating_name', ['name' => trans($formConfig->name)])); ?>"
+        data-request-before-update="$el.trigger('unchange.oc.changeMonitor')"
+        class="btn btn-default">
+        <?= e(trans('backend::lang.form.create_and_close')); ?>
+    </button>
+    <span class="btn-text">
+        <?= e(trans('backend::lang.form.or')) ?> <a href="<?= Backend::url($formConfig->defaultRedirect) ?>"><?= e(trans('backend::lang.form.cancel')); ?></a>
+    </span>
+</div>
