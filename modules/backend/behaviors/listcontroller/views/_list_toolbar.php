fe0abe71e69c379df6213e68e9712d7a5414153d
Luke Towers <git@luketowers.ca>
2025-05-06 10:23:22 -0600

    Update modules/backend/behaviors/listcontroller/views/_list_toolbar.php

--- modules/backend/behaviors/listcontroller/views/_list_toolbar.php
+++ modules/backend/behaviors/listcontroller/views/_list_toolbar.php
@@ -10,19 +10,20 @@ $listConfig = $listController->getConfig();
         <?= e(trans('backend::lang.form.create_title', ['name' => trans(\Winter\Storm\Support\Str::before($listConfig->title, '_plural'))])); ?>
     </a>
 
-  <?php if (isset($listConfig->showCheckboxes) && $listConfig->showCheckboxes != false): ?>
-    <button
-        class="btn btn-danger wn-icon-trash-o"
-        disabled="disabled"
-        onclick="$(this).data('request-data', { checked: $('.control-list').listWidget('getChecked') })"
-        data-request="onDelete"
-        data-request-confirm="<?= e(trans('backend::lang.list.delete_selected_confirm')); ?>"
-        data-trigger-action="enable"
-        data-trigger=".control-list input[type=checkbox]"
-        data-trigger-condition="checked"
-        data-request-success="$(this).prop('disabled', 'disabled')"
-        data-stripe-load-indicator>
-        <?= e(trans('backend::lang.list.delete_selected')); ?>
-    </button>
-  <?php endif ?>
+    <?php if (isset($listConfig->showCheckboxes) && $listConfig->showCheckboxes != false): ?>
+        <button
+            class="btn btn-danger wn-icon-trash-o"
+            disabled="disabled"
+            onclick="$(this).data('request-data', { checked: $('.control-list').listWidget('getChecked') })"
+            data-request="onDelete"
+            data-request-confirm="<?= e(trans('backend::lang.list.delete_selected_confirm')); ?>"
+            data-trigger-action="enable"
+            data-trigger=".control-list input[type=checkbox]"
+            data-trigger-condition="checked"
+            data-request-success="$(this).prop('disabled', 'disabled')"
+            data-stripe-load-indicator
+        >
+            <?= e(trans('backend::lang.list.delete_selected')); ?>
+        </button>
+    <?php endif ?>
 </div>
