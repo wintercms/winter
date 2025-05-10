b05011a8b018671d0d28b8890275b964a3b0d7b4
Luke Towers <git@luketowers.ca>
2025-05-10 00:47:22 -0600

    Update modules/backend/behaviors/formcontroller/partials/_toolbar.php

--- modules/backend/behaviors/formcontroller/partials/_toolbar.php
+++ modules/backend/behaviors/formcontroller/partials/_toolbar.php
@@ -74,7 +74,6 @@
             class="wn-icon-trash-o btn-icon danger pull-right"
         >
         </button>
-
         <span class="btn-text">
             <?= e(trans('backend::lang.form.or')) ?> <a href="<?= Backend::url($formConfig->defaultRedirect) ?>"><?= e(trans('backend::lang.form.cancel')); ?></a>
         </span>
