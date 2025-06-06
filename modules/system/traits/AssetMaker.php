991f6285ea776e9bd2759f57df292de5a1a72206
Jack Wilkinson <31214002+jaxwilko@users.noreply.github.com>
2025-06-06 11:08:10 +0100

    Added fix for multiple vite assets being deleted as duplicate

--- modules/system/traits/AssetMaker.php
+++ modules/system/traits/AssetMaker.php
@@ -399,12 +399,31 @@ trait AssetMaker
                     continue;
                 }
 
-                if (isset($pathCache[$path])) {
-                    array_forget($collection, $key);
+                if ($type !== 'vite') {
+                    if (isset($pathCache[$path])) {
+                        array_forget($collection, $key);
+                        continue;
+                    }
+
+                    $pathCache[$path] = true;
                     continue;
                 }
 
-                $pathCache[$path] = true;
+                // If handling vite, ensure that each entrypoint is considered it's own path within a package
+                foreach ($asset['attributes']['entrypoints'] ?? [] as $index => $entrypoint) {
+                    $vitePath = $path . '|' . $entrypoint;
+                    // If we detect a duplicate, remove it
+                    if (isset($pathCache[$vitePath])) {
+                        array_forget($collection[$key]['attributes']['entrypoints'], $index);
+                        // If all entry points have been removed from an asset, then remove it
+                        if (empty($collection[$key]['attributes']['entrypoints'])) {
+                            unset($collection[$key]);
+                        }
+                        continue;
+                    }
+
+                    $pathCache[$vitePath] = true;
+                }
             }
         }
     }
