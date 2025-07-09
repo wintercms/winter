178b4dd6f2b213e42b6c965252e6286eacd698fd
Jack Wilkinson <31214002+jaxwilko@users.noreply.github.com>
2025-07-09 15:42:37 +0100

    Add fix to unify asset:compile & asset:watch arguments

--- modules/system/console/asset/AssetCompile.php
+++ modules/system/console/asset/AssetCompile.php
@@ -126,15 +126,16 @@ abstract class AssetCompile extends Command
 
         $packages = $compilableAssets->getPackages($type);
         $name = $this->argument('package');
+        $nameLower = strtolower($name);
 
-        if (!in_array($name, array_keys($packages))) {
+        if (!in_array($nameLower, array_keys($packages))) {
             $this->error(
                 sprintf('Package "%s" is not a registered package.', $name)
             );
             return 1;
         }
 
-        $package = $packages[$name];
+        $package = $packages[$nameLower];
 
         $relativeConfigPath = $package['config'];
         if (!$this->isPackageWithinWorkspace($relativeConfigPath)) {
