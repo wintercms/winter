76b57bccbb14b957b7981e43e0a5a969db220607
Luke Towers <git@luketowers.ca>
2026-02-20 01:39:02 -0600

    Simplify FormController context identification logic

--- modules/backend/behaviors/FormController.php
+++ modules/backend/behaviors/FormController.php
@@ -122,11 +122,7 @@ class FormController extends ControllerBehavior
      */
     public function initForm($model, $context = null)
     {
-        if ($context !== null) {
-            $this->context = $context;
-        }
-
-        $context = $this->formGetContext();
+        $context = $this->context = $context ?? $this->formGetContext();
 
         /*
          * Each page can supply a unique form definition, if desired
@@ -440,7 +436,7 @@ class FormController extends ControllerBehavior
      */
     public function formGetContext()
     {
-        return post('form_context', $this->context);
+        return $this->context;
     }
 
     /**
